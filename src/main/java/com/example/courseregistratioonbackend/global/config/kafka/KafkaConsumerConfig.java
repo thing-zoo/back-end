91b04cc7873e72dd3fee42e6a60b732d3eea3492
thing-zoo <cathy4025@gmail.com>
2023-08-21 19:56:19 +0900

    feat: Kafka 설정 추가

---
+++ src/main/java/com/example/courseregistratioonbackend/global/config/kafka/KafkaConsumerConfig.java
@@ -0,0 +1,74 @@
+package com.example.courseregistratioonbackend.global.config.kafka;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.KafkaListenerErrorHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@EnableKafka
+@Configuration
+public class KafkaConsumerConfig {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapAddress;
+
+    @Value("${spring.kafka.consumer.group-id}")
+    private String groupId;
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String>
+    kafkaListenerContainerFactory() {
+
+        ConcurrentKafkaListenerContainerFactory<String, String> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
+
+    @Bean
+    public ConsumerFactory<String, String> consumerFactory() {
+        return new DefaultKafkaConsumerFactory<>(consumerConfigs());
+    }
+
+    @Bean
+    public Map<String, Object> consumerConfigs() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(
+                ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                bootstrapAddress);
+        props.put(
+                ConsumerConfig.GROUP_ID_CONFIG,
+                groupId);
+        props.put(
+                ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
+                StringDeserializer.class);
+        props.put(
+                ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
+                StringDeserializer.class);
+        return props;
+    }
+
+    @Bean
+    public KafkaListenerErrorHandler kafkaErrorHandler() {
+        return (msg, ex) -> {
+            log.error("[KafkaErrorHandler] kafkaMessage=[" + msg.getPayload() + "], errorMessage=[" + ex.getMessage() + "]");
+
+            ConsumerRecord<String, String> record = (ConsumerRecord<String, String>) msg.getPayload();
+            // 메시지를 더 가공하거나 별도 처리
+
+            return record.value();  // sendTo("토픽명")에 입력된 토픽으로 전달 될 메시지 내용
+        };
+    }
+}
